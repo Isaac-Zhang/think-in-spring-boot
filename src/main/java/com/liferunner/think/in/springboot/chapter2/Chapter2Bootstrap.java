24e10354568ac106555eb78e8a7acbc787013cca
Isaac Zhang <zhangpan0614@126.com>
2019-10-29 22:00:19 +0800

    use undertow start webflux

--- src/main/java/com/liferunner/think/in/springboot/chapter2/Chapter2Bootstrap.java
+++ src/main/java/com/liferunner/think/in/springboot/chapter2/Chapter2Bootstrap.java
@@ -2,8 +2,16 @@ package com.liferunner.think.in.springboot.chapter2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 /**
  * Chapter2Bootstrap for : TODO
@@ -22,5 +30,12 @@ public class Chapter2Bootstrap {
 
     public static void main(String[] args) {
         SpringApplication.run(Chapter2Bootstrap.class, args);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> hello(){
+        return route(GET("/hello"),
+                request ->ok().body(Mono.just("Hello,webflux"),String.class)
+        );
     }
 }
