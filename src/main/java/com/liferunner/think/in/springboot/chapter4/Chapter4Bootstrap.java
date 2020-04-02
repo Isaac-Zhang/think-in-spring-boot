f490dbf18d8122824fc96b3ff9472866bf938fea
Isaac Zhang <zhangpan0614@126.com>
2020-04-02 11:16:19 +0800

    转换Springboot ApplicationContext的各种实现接口

--- src/main/java/com/liferunner/think/in/springboot/chapter4/Chapter4Bootstrap.java
+++ src/main/java/com/liferunner/think/in/springboot/chapter4/Chapter4Bootstrap.java
@@ -1,10 +1,12 @@
 package com.liferunner.think.in.springboot.chapter4;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.context.WebServerApplicationContext;
 import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,27 +35,36 @@ public class Chapter4Bootstrap {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Chapter4Bootstrap.class, args);
+        WebServerApplicationContext applicationContext = (WebServerApplicationContext) SpringApplication
+            .run(Chapter4Bootstrap.class,
+                args);
+        System.out.println("==========当前WebServer：" + applicationContext.getWebServer().getClass().getName());
     }
 
+    /**
+     * WebFlux 函数式端点（endpoint）代码
+     */
     @Bean
     public RouterFunction<ServerResponse> hello() {
         return route(GET("/hello"),
-                request -> ok().body(Mono.just("Hello,webflux"), String.class)
+            request -> ok().body(Mono.just("Hello,webflux"), String.class)
         );
     }
 
+    /**
+     * {@link ApplicationRunner#run(ApplicationArguments)} 方法在Spring Boot 应用启动后调用
+     */
     @Bean
     public ApplicationRunner runner(WebServerApplicationContext context) {
         return (args -> {
             System.out.println("当前Web server实现类为：" +
-                    context.getWebServer().getClass().getName());
+                context.getWebServer().getClass().getName());
         });
     }
 
     @EventListener(WebServerInitializedEvent.class)
     public void onWebServerReady(WebServerInitializedEvent event) {
         System.out.println("当前WebServer实现类为：" +
-                event.getWebServer().getClass().getName());
+            event.getWebServer().getClass().getName());
     }
 }
