2a5c0d02593db538fea2077fa2988e6b72539c7e
Isaac Zhang <zhangpan0614@126.com>
2020-04-03 09:50:41 +0800

    2.2.6 actuator had defects.

---
+++ chapter6/src/main/java/com/liferunner/think/in/springboot/Chapter6Application.java
@@ -0,0 +1,18 @@
+package com.liferunner.think.in.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Chapter6Application for TODO
+ *
+ * @author <a href="mailto:magicianisaac@gmail.com">Isaac.Zhang | 若初</a>
+ * @since 2020/4/2
+ **/
+@SpringBootApplication
+public class Chapter6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter6Application.class,args);
+    }
+}
