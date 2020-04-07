cdda4911fa903ff94bccbfa2f663994cc883e12b
Isaac Zhang <zhangpan0614@126.com>
2020-04-07 22:42:08 +0800

    利用反射获取 注解 属性值

--- chapter7/src/main/java/com/liferunner/thinking/in/springboot/TransactionalServiceAnnotationReflectionApplication.java
+++ chapter7/src/main/java/com/liferunner/thinking/in/springboot/TransactionalServiceAnnotationReflectionApplication.java
@@ -1,7 +1,15 @@
 package com.liferunner.thinking.in.springboot;
 
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Target;
 import java.lang.reflect.AnnotatedElement;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.springframework.util.ReflectionUtils;
+import org.springframework.util.StringUtils;
 
 /**
  * TransactionalServiceAnnotationReflectionApplication for 利用反射获取 {@link TransactionalService} name()
@@ -26,7 +34,42 @@ public class TransactionalServiceAnnotationReflectionApplication {
                 method.getName(),//执行Method 反射
                 ReflectionUtils.invokeMethod(method, transactionalService)
             )
-            , method -> method.getParameterCount() == 0);// 选择无参数方法
+//            , method -> method.getParameterCount() == 0);// 选择无参数方法
+            , method -> !method.getDeclaringClass().equals(Annotation.class)); // 因为所有的注解实现了Annotation接口，因此要过滤掉
 
+        // 获取 TransactionalService 的所有元注解
+        Set<Annotation> annotationSet = getAllMetaAnnotations(transactionalService);
+        annotationSet.forEach(annotation -> {
+            ReflectionUtils.doWithMethods(annotation.annotationType(),
+                method -> System.out.printf("反射获取: %s.%s = %s\n",
+                    annotation.annotationType().getSimpleName(),
+                    method.getName(),//执行Method 反射
+                    ReflectionUtils.invokeMethod(method, annotation)
+                )
+                , method -> !method.getDeclaringClass().equals(Annotation.class)); // 因为所有的注解实现了Annotation接口，因此要过滤掉
+
+        });
+    }
+
+    /**
+     * 获取 annotation 的所有元注解
+     */
+    private static Set<Annotation> getAllMetaAnnotations(Annotation annotation) {
+        Annotation[] annotations = annotation.annotationType().getAnnotations();
+        if (StringUtils.isEmpty(annotation)) {
+            return Collections.EMPTY_SET;
+        }
+
+        //获取所有非 Java 标准元注解集合
+        Set<Annotation> annotationSet = Arrays.stream(annotations).filter(mt -> !Target.class.getPackage().equals(
+            mt.annotationType().getPackage()
+        )).collect(Collectors.toSet());
+        // 递归查找注解上的元注解集合
+        Set<Annotation> mtaSet = annotationSet.stream().map(
+            TransactionalServiceAnnotationReflectionApplication::getAllMetaAnnotations
+        ).collect(HashSet::new, Set::addAll, Set::addAll);
+
+        annotationSet.addAll(mtaSet);
+        return annotationSet;
     }
 }
