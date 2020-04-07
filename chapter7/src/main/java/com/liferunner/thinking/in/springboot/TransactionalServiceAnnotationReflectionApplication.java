ecd51ee886bca52ded9e1cd5fb79ebb312d0652a
Isaac Zhang <zhangpan0614@126.com>
2020-04-07 22:21:45 +0800

    利用反射获取 注解 属性值

--- chapter7/src/main/java/com/liferunner/thinking/in/springboot/TransactionalServiceAnnotationReflectionApplication.java
+++ chapter7/src/main/java/com/liferunner/thinking/in/springboot/TransactionalServiceAnnotationReflectionApplication.java
@@ -1,10 +1,10 @@
 package com.liferunner.thinking.in.springboot;
 
 import java.lang.reflect.AnnotatedElement;
+import org.springframework.util.ReflectionUtils;
 
 /**
- * TransactionalServiceAnnotationReflectionApplication for
- * 利用反射获取 {@link TransactionalService} name()
+ * TransactionalServiceAnnotationReflectionApplication for 利用反射获取 {@link TransactionalService} name()
  *
  * @author <a href="mailto:magicianisaac@gmail.com">Isaac.Zhang | 若初</a>
  * @since 2020/4/7
@@ -16,8 +16,17 @@ public class TransactionalServiceAnnotationReflectionApplication {
         //  Class 实现了 AnnotatedElement 接口
         AnnotatedElement annotatedElement = TransactionalServiceAnnotationReflectionApplication.class;
 
-        String name = annotatedElement.getAnnotation(TransactionalService.class).name();
+        TransactionalService transactionalService = annotatedElement.getAnnotation(TransactionalService.class);
 
-        System.out.println(name);
+        System.out.println("显示调用name方法：" + transactionalService.name());
+
+        // 完全 Java 反射实现（ReflectionUtils 为 Spring 的反射工具类）
+        ReflectionUtils.doWithMethods(TransactionalService.class,
+            method -> System.out.printf("反射获取: %s = %s\n",
+                method.getName(),//执行Method 反射
+                ReflectionUtils.invokeMethod(method, transactionalService)
+            )
+            , method -> method.getParameterCount() == 0);// 选择无参数方法
+
     }
 }
